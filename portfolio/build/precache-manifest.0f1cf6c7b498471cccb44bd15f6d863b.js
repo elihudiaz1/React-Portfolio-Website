35ad49c32e443e83d5de260497c0a918365dcc76
Elihu Diaz <diazelihu27@gmail.com>
2020-05-23 13:13:16 -0400

    created build file

---
+++ portfolio/build/precache-manifest.0f1cf6c7b498471cccb44bd15f6d863b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d697af6568c3f4a3e44956c408dd14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9ac4b873aa9e4ca6b1",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "fcef7451a09d63f1f886",
+    "url": "/static/css/main.9d6d091b.chunk.css"
+  },
+  {
+    "revision": "8e9ac4b873aa9e4ca6b1",
+    "url": "/static/js/2.f1e7bf99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f1e7bf99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcef7451a09d63f1f886",
+    "url": "/static/js/main.a90664eb.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "a9eed18bda5b095be20ba02d0b83a9e0",
+    "url": "/static/media/paper.a9eed18b.jpg"
+  },
+  {
+    "revision": "91f4a0973bd22d8f89aaabe1d27f281a",
+    "url": "/static/media/parking-garage.91f4a097.jpg"
+  },
+  {
+    "revision": "34b05bde428de740f856aa1aa3b121c9",
+    "url": "/static/media/profilepic.34b05bde.png"
+  }
+]);
